dcc0b316fac01a1fae8c299163bc2e789e3ad53a
Raniro Coelho <errecme@gmail.com>
2017-09-27 20:53:11 -0300

    Setup Schemas

--- models/itemModel.js
+++ models/itemModel.js
@@ -4,17 +4,33 @@ var Schema = mongoose.Schema;
 
 var itemSchema = new Schema({
   code: String,
-  description: String,
-  quantity: String,
-  quantitySold: String,
-  purchaseValue: String,
-  soldValue: String,
-  discountTotal: String
+  brand: String,
+  description: {
+    category: String,
+    subcategory: String,
+    about: String,
+    color : String,
+    sex: String,
+    size: String
+  },
+  quantity: {
+    bought: String,
+    sold: String
+  },
+  value: {
+    bought: String,
+    sold: String,
+    tag: String
+  }
 });
 
 var financeSchema = new Schema({
   totalSold: String,
-  totalItemsSold: String
+  totalItemsSold: String,
+  discount: {
+    code: String,
+    quantity: String
+  }
 });
 
 var Store = {Items: mongoose.model('Items', itemSchema), Finance: mongoose.model('Finance', financeSchema)};
