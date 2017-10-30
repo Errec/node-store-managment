156de4d76d8f78540ae495e35c3ea512a1909a27
Raniro Coelho <errecme@gmail.com>
2017-10-30 20:58:31 -0200

    Set header btn color focus on intro btn click

--- front-end/js/intro.js
+++ front-end/js/intro.js
@@ -1,11 +1,23 @@
 var intro = (function() {
-   var introLogo     = document.querySelector('.intro__logo');
-   var introItem     = document.querySelector('.intro__item');
-   var introFinances = document.querySelector('.intro__finances');
-   var header        = document.querySelector('.header');
+   var introLogo        = document.querySelector('.intro__logo');
+   var introItem        = document.querySelector('.intro__item');
+   var introItemBtn     = document.querySelector('.header__item');
+   var introFinances    = document.querySelector('.intro__finances');
+   var introFinancesBtn = document.querySelector('.header__finances');
+   var header           = document.querySelector('.header');
 
-   introItem.addEventListener('click', _hideIntro);
-   introFinances.addEventListener('click', _hideIntro);
+   introItem.addEventListener('click', _openItem);
+   introFinances.addEventListener('click', _openFinances);
+
+   function _openItem() {
+    introItemBtn.classList.add('header__btn--green-border');
+    _hideIntro();
+   }
+
+   function _openFinances() {
+    introFinancesBtn.classList.add('header__btn--green-border');
+    _hideIntro();
+   }
 
    function _hideIntro() {
     introLogo.classList.add('intro__logo--hide');
